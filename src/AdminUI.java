bda23918ec264ddc6ee82cf5ad923751a2fee3e6
jrfranklin <jrfranklin@lin4.cs.csbsju.edu>
2016-03-13 19:02:02 -0500

    sup

---
+++ src/AdminUI.java
@@ -0,0 +1,49 @@
+
+public interface AdminUI {
+	private User u;
+	
+	public void manageUsers(User m){
+		
+	}
+	
+	public void addUser(){
+		User u = new User;
+	}
+	
+	public void deactivateUser(){
+		
+	}
+	
+	public void editUser(User m){
+		
+	}
+
+	public void manageUniversities(){
+		
+	}
+	
+	public boolean addSchool(){
+		
+	}
+	
+	public boolean saveAddedUniversities(){
+		
+	}
+	
+	public void editUniversities(University u){
+		
+	}
+	
+	public void applyChanges(){
+		
+	}
+	
+	public void resetUniversity(){
+		
+	}
+	
+	public void resetUser(){
+		
+	}
+	
+}
