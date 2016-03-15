510777a8dbda6da89a79f957a1ed62e426cedbec
jshinnenkamp <jshinnenkamp@lin10.cs.csbsju.edu>
2016-03-14 20:57:20 -0500

    user class with javadoc

--- src/User.java
+++ src/User.java
@@ -3,14 +3,20 @@ package project;
 import java.util.*;
 
 public class User {
-
+    /** password for the user */
 	private String password;
+	/** username for the user */
 	private String username;
+	/** firstName of the user*/
 	private String firstName;
+	/** lastName of the user*/
 	private String lastName;
+	/** type of the user*/
 	private char type;
+	/** status of the user*/
 	private char status;
 	
+	/** constructor */
 	public User(String pw, String un, String firstName, String lastName, char type, char status) {
 		this.password = pw;
 		this.username = un;
@@ -20,50 +26,47 @@ public class User {
 		this.status = status;
 	}
 	
-	public void logOn() {
-		logOn(username, password); //???????????????????/
-	}
-	
-	public void logOff() {
-		//????????????????????????????????????????????????????????
-	}
-	
-	//FIRST NAME
+	/** @return the users firstName*/
 	public String getFirstName() {
 		return firstName;
 	}
+	/** @param firstName to be set to user*/
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	//LAST NAME
+	/** @return the users lastName */
 	public String getLastName() {
 		return lastName;
 	}
+	/** @param lastName to be set to user */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	//USERNAME
+	/** @return the users username */
 	public String getUsername() {
 		return username;
 	}
+	/** @param username to be set to the user */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-	//PASSWORD
+	/** @return the user password */
 	public String getPassword() {
 		return password;
 	}
+	/** @param password to be set to the user*/
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	//TYPE
+	/** @return the users type*/
 	public char getType() {
 		return type;
 	}
+	/** @param type to be set to the user */
 	public void setType(char type) {
 		this.type = type;
 	}
